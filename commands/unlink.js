204cccb59a182e3bb7520a0efd2904f9ba3a5f94
FelixKLG <felix@felixklg.dev>
2021-12-09 20:53:30 +0000

    unlink command

---
+++ commands/unlink.js
@@ -0,0 +1,49 @@
+const axios = require('axios');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { captureException } = require('@sentry/node');
+
+const LinkURL = process.env.API_URL;
+const LinkAPIKEY = process.env.API_KEY;
+const MemberROLE = process.env.VERIFIED_MEMBER_ROLE;
+const SentryEnabled = process.env.SENTRY_ENABLED;
+
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('unlink')
+		.setDescription('Unlink the mentioned user.')
+		.setDefaultPermission(false)
+		.addUserOption(option =>
+			option.setName('member')
+				.setDescription('The user to unlink.')
+				.setRequired(true)),
+	async execute(interaction) {
+
+		const httpClient = await axios.create({
+			baseURL: LinkURL,
+			timeout: 3000,
+			headers: { 'Key': LinkAPIKEY },
+		});
+
+		const mentionedUser = await interaction.options.getUser('member');
+
+		await httpClient.get('api/unlink', {
+			data: {
+				'id': mentionedUser.id,
+			},
+		}).then(async function(response) {
+			if (response.status === 200) {
+				await mentionedUser.member.roles.remove(MemberROLE);
+				await interaction.reply('member unlinked');
+			}
+			else if (response.status === 404) {
+				await interaction.reply('member is not linked');
+			}
+		}).catch(async function(error) {
+			if (error.response.status !== 404 && SentryEnabled) {
+				captureException(error);
+			}
+		});
+
+	},
+};

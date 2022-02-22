7823adfb34fd9e97a401c50f025f699586e39c1f
Hayden D <exehad701@gmail.com>
2022-02-22 14:50:26 -0500

    brok more shit

--- commands/kick.js
+++ commands/kick.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { DMChannel, Message, Guild } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -12,11 +13,19 @@ module.exports = {
 			option.setName('reason')
 				.setDescription('The reason this member is getting kicked')
 				.setRequired(false))
-		.setDefaultPermission(false),
+		.setDefaultPermission(true),
 	async execute(interaction) {
 
 		const memberPerms = await interaction.memberPermissions.has('KICK_MEMBERS');
-		const target = await interaction.options.getUser('member');
+		const target = await interaction.options.getMember('member');
+		let reason;
+
+		if (interaction.options.getString('reason') === null) {
+			reason = 'No reason provided';
+		}
+		else {
+			reason = interaction.options.getString('reason');
+		}
 
 		try {
 			if (memberPerms) {
@@ -24,21 +33,24 @@ module.exports = {
 			}
 			else {
 				interaction.reply({ content: 'You do not have permission', ephemeral: true });
+				return;
 			}
 		}
 		catch (e) {
 			console.error(e);
 		}
 
-		async function kickMember() {
-			try {
-				target.kick('Random');
-			}
-			catch (e) {
-				console.error(e);
-			}
+		async function msgMember() {
+			target.send('You have been kicked from ' + Guild.name + '\n Reason: ' + reason);
 		}
 
+		async function kickMember() {
+			// eslint-disable-next-line quotes
+			target.kick(reason);
+		}
+
+		await msgMember();
+		await kickMember();
 		interaction.reply({ content: 'User was kicked successfully', ephemeral: true });
 	},
 };
